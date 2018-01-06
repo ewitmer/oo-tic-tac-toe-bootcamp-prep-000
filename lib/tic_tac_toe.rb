1207765362165a9cc2f4f1ecbc27186f762a6e46
Erin Witmer <ecwitmer@gmail.com>
2018-01-06 01:13:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,16 +71,16 @@ class TicTacToe
   end
   
   def won?
-  WIN_COMBINATIONS.each do |combo|
-    if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
-      return combo
+    WIN_COMBINATIONS.each do |combo|
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+        return combo
+      end
+      if @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        return combo
+      end
     end
-    if @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-      return combo
-    end
-  end
-  return false
-
+    return false
   
-end
+    
+  end
 end
